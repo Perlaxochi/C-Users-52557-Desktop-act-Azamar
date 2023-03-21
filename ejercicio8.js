90d619dd2570e90e057d9a92f6e224b949acff8e
Perlaxochi <hernandezperla1314@gmail.com>
2023-03-21 11:21:42 -0600

    Se agrega el proyecto

---
+++ ejercicio8.js
@@ -0,0 +1,16 @@
+//let suma = function(a,b){
+//    return a+b;
+//}
+//console.log(suma(12,12));
+
+//node  .\ejercicio8.js ---> para ingresar al archivo y ejecutarlo
+
+//APUNTADOR FLECHA
+//let suma = (a,b) =>{
+  //  return a+b
+//}
+//console.log(suma(222,12));
+
+let suma = (a,b) => a+b;
+console.log(suma(313,3));
+//constructor
